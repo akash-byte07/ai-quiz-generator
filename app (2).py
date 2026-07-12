4a1eb72dfcf4e2264acd5955e5c036c99768e882
akash-byte07 <akashpatil8001@gmail.com>
2026-07-13 00:56:38 +0530

    fix render

--- app (2).py
+++ app (2).py
@@ -357,4 +357,4 @@ with gr.Blocks(
         ],
     )
 
-demo.launch(share=True, debug=True)
+demo.launch( server_name="0.0.0.0", server_port=int(os.environ.get("PORT",7860)),share=True, debug=True)
